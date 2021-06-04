ebd9a5f4d1f3234bcc61353a652f626b32f43182
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-06-04 20:17:43 +0700

    Test commit

---
+++ src/screens/IntroductionCarouselScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const IntroductionCarouselScreen = () => {
+  return (
+    <View>
+      <Text>Carousel Screen</Text>
+    </View>
+  );
+};
+
+export default IntroductionCarouselScreen;
