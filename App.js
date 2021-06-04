5a824691c84f45f00cc1a6619a0a57886d35a9ce
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-06-05 00:22:42 +0700

    Added Boilerplate for Navigator in App.js

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {View, Text, Button} from 'react-native';
+import IntroductionCarouselScreen from './src/screens/IntroductionCarouselScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          component={IntroductionCarouselScreen}
+          name="Welcome"
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
