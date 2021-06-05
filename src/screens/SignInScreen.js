8fdb90ed1fbd22e26c1d8cc2f2e69f8a54c14f61
Reinaldo Usadani <reinaldo.bbu20@gmail.com>
2021-06-06 03:15:58 +0700

    Fixed many visual bugs on Signin and Signup

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,66 +1,109 @@
-import React from 'react';
-import {View, TouchableOpacity} from 'react-native';
-import {Image, Input, Button, Text} from 'react-native-elements';
+import React, {useState} from 'react';
+import {Image, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {Input, Text} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const SignInScreen = ({navigation}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#003049',
-      }}>
-
-      <Image
-        source={require('../../assets/logo.png')}
-        style={{height: 215, width: 195, marginBottom: 15}}
-      />
+    <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.logo} />
 
       <Input
         placeholder="Username"
         leftIcon={<Icon name="person" size={24} color="white" />}
-        containerStyle={{paddingHorizontal: 50}}
-        style={{color: 'white'}}
+        containerStyle={styles.inputContainer}
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={username}
+        onChangeText={newUsername => setUsername(newUsername)}
       />
       <Input
         placeholder="Password"
         secureTextEntry={true}
         leftIcon={<Icon name="key" size={24} color="white" />}
-        containerStyle={{paddingHorizontal: 50}}
-        style={{color: 'white'}}
-      />
-      
-      <Button
-        title="Masuk"
-        buttonStyle={{ backgroundColor: '#F77F00', width: 110, height: 40, borderRadius: 10, marginTop: 5 }}
-        titleStyle={{ fontFamily: 'Poppins-Black' }}
-        onPress={() => alert('Masuk Pressed!')}
+        containerStyle={styles.inputContainer}
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={newPassword => setPassword(newPassword)}
       />
 
-      <Text style={{color: 'white', fontFamily: 'Poppins-Black', marginVertical: 10, fontSize: 16 }}>atau</Text>
-
-      <Button
-        icon={
-          <Image
-            source={require('../../assets/google.png')}
-            style={{height: 25, width: 25}}
-          />
-        }
-        iconLeft
-        title="Masuk pake gugle"
-        buttonStyle={{ backgroundColor: '#F77F00', height: 40, borderRadius: 10 }}
-        titleStyle={{ fontFamily: 'Poppins-Black', marginHorizontal: 20 }}
-        onPress={() => alert('Masuk with Google Pressed!')}
-      />
-
-      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-        <Text style={{color: 'white', fontFamily: 'Poppins-Regular', marginTop: 30, textDecorationLine: 'underline' }}>Belom punya akun? Klik sini buat daftar!</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => alert(`username: ${username} \npassword: ${password}`)}>
+        <Text
+          style={{color: 'white', fontFamily: 'Poppins-Black', fontSize: 16}}>
+          Masuk
+        </Text>
       </TouchableOpacity>
 
+      <Text
+        style={{
+          color: 'white',
+          fontFamily: 'Poppins-Black',
+          marginVertical: 10,
+          fontSize: 16,
+        }}>
+        atau
+      </Text>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image source={require('../../assets/google.png')} />
+        <TouchableOpacity style={{...styles.button, marginLeft: 20}}>
+          <Text
+            style={{color: 'white', fontFamily: 'Poppins-Black', fontSize: 16}}>
+            Masuk pake gugle
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+        <Text style={styles.navLink}>
+          Belom punya akun? Tekan sini buat daftar!
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#003049',
+  },
+  logo: {
+    height: 215,
+    width: 195,
+    marginBottom: 15,
+    resizeMode: 'contain',
+  },
+  inputContainer: {paddingHorizontal: 50},
+  input: {color: 'white', fontFamily: 'Poppins-Regular'},
+  button: {
+    backgroundColor: '#ff7f00',
+    paddingVertical: 7,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+    shadowColor: 'black',
+    elevation: 6,
+  },
+  navLink: {
+    color: 'white',
+    fontFamily: 'Poppins-Regular',
+    marginTop: 30,
+    textDecorationLine: 'underline',
+  },
+});
 
 export default SignInScreen;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,57 +1,87 @@
-import React from 'react';
-import {View, TouchableOpacity} from 'react-native';
-import {Image, Input, Button, Text} from 'react-native-elements';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {Input, Button, Text} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const SignUpScreen = ({navigation}) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#003049',
-      }}>
-
-      <Image
-        source={require('../../assets/logo.png')}
-        style={{height: 215, width: 195, marginBottom: 15}}
-      />
+    <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.logo} />
 
       <Input
         placeholder="Username"
         leftIcon={<Icon name="person" size={24} color="white" />}
-        containerStyle={{paddingHorizontal: 50}}
-        style={{color: 'white'}}
+        containerStyle={styles.inputContainer}
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <Input
         placeholder="Password"
         secureTextEntry={true}
         leftIcon={<Icon name="key" size={24} color="white" />}
-        containerStyle={{paddingHorizontal: 50}}
-        style={{color: 'white'}}
+        containerStyle={styles.inputContainer}
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <Input
         placeholder="Confirm Password"
         secureTextEntry={true}
         leftIcon={<Icon name="key" size={24} color="white" />}
-        containerStyle={{paddingHorizontal: 50}}
-        style={{color: 'white'}}
+        containerStyle={styles.inputContainer}
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
-      
-      <Button
-        title="Daftar"
-        buttonStyle={{ backgroundColor: '#F77F00', width: 110, height: 40, borderRadius: 10, marginTop: 5 }}
-        titleStyle={{ fontFamily: 'Poppins-Black' }}
-        onPress={() => alert('Daftar Pressed!')}
-      />
-      
-      <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
-        <Text style={{color: 'white', fontFamily: 'Poppins-Regular', marginTop: 30, textDecorationLine: 'underline' }}>Udah punya akun? Klik sini buat masuk!</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => alert('Daftar Pressed')}>
+        <Text
+          style={{color: 'white', fontFamily: 'Poppins-Black', fontSize: 16}}>
+          Daftar
+        </Text>
       </TouchableOpacity>
 
+      <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+        <Text style={styles.navLink}>
+          Udah punya akun? Tekan sini buat masuk!
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#003049',
+  },
+  logo: {
+    height: 215,
+    width: 195,
+    marginBottom: 15,
+    resizeMode: 'contain',
+  },
+  inputContainer: {paddingHorizontal: 50},
+  input: {color: 'white', fontFamily: 'Poppins-Regular'},
+  button: {
+    backgroundColor: '#ff7f00',
+    paddingVertical: 7,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+    shadowColor: 'black',
+    elevation: 6,
+  },
+  navLink: {
+    color: 'white',
+    fontFamily: 'Poppins-Regular',
+    marginTop: 25,
+    textDecorationLine: 'underline',
+  },
+});
 
 export default SignUpScreen;
