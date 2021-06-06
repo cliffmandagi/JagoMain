9aae2f1f09da1a0c12507e7559737f54840c0f9c
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2021-06-06 23:06:43 +0800

    Update Home

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {View, Text, Button} from 'react-native';
+import {View, Image, Text, Button} from 'react-native';
 import IntroductionCarouselScreen from './src/screens/IntroductionCarouselScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
@@ -9,8 +9,69 @@ import HomeScreen from './src/screens/HomeScreen';
 import {Provider as AuthProvider} from './src/context/AuthContext';
 import {setNavigator} from './src/navigationRef';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Context as AuthContext} from './src/context/AuthContext';
 
 const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => {
+  const {state} = useContext(AuthContext);
+  const {user} = state;
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#F77F00"
+      barStyle={{ backgroundColor: '#003049' }}
+    >
+      <Tab.Screen
+        name="Berita"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tournament"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Turnamen',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-trophy" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tutorial"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Panduan',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-book" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Profil',
+          tabBarIcon: ({ color }) => (
+            // <Icon name="ios-person" color={color} size={26} />
+            <Image
+              source={{uri: user.photo}}
+              style={{width: 26, height: 26, borderRadius: 50, borderWidth: 1, borderColor: `${color}`}}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+    )
+}
 
 const App = () => {
   useEffect(() => {
@@ -45,7 +106,7 @@ const App = () => {
             }}
           />
           <Stack.Screen
-            component={HomeScreen}
+            component={MainTabScreen}
             name="Home"
             options={{
               headerShown: false,
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,32 @@
 import React, {useContext} from 'react';
-import {View, StyleSheet, Image} from 'react-native';
-import {Text} from 'react-native-elements';
-import {Input} from 'react-native-elements';
+import {View, StyleSheet, ScrollView, Image} from 'react-native';
+import {Text, Header, Divider} from 'react-native-elements';
 import {Context as AuthContext} from '../context/AuthContext';
 
 const HomeScreen = () => {
   const {state} = useContext(AuthContext);
   const {user} = state;
   return (
-    <View style={styles.container}>
-      <Text h2>Hi {user.givenName}</Text>
-      <Image
-        source={{uri: user.photo}}
-        style={{width: 100, height: 100, borderRadius: 50, marginTop: 20}}
-      />
+    <View style={{flex: 1, backgroundColor: '#002437'}}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header
+          placement="left"
+          centerComponent={{
+            text: 'Jagomain',
+            style: {color: '#fff', fontFamily: 'Poppins-Bold', fontSize: 18},
+          }}
+          backgroundColor="#003049"
+          containerStyle={{borderBottomColor: '#002437'}}
+        />
+
+        <View style={{flexDirection: 'row'}}>
+          {/* <Image source={require('../../assets/fire.png')}/> */}
+          <Text style={{color: 'white', fontFamily: 'Poppins-Black', fontSize: 18}}>TEST</Text>
+        </View>
+
+        <Divider style={{ backgroundColor: '#00283C', marginHorizontal: 20 }} />
+
+      </ScrollView>
     </View>
   );
 };
